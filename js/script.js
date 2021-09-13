3271fcb4a11cff1860528a0eab4964eb160c0328
alfarukhan <64387854+alfarukhan@users.noreply.github.com>
2021-09-13 13:43:23 +0700

    Animation Adding

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-//event//
-$('.page-scroll').on('click', function(e) {
-
+// JavaScript Document//event//
+$('.page-scroll').on('click',function(e) {
 //ambil isi
  var tujuan = $(this).attr('href');
 //tangkap elemen ybs
  var elemenTujuan = $(tujuan);
-
-	
 //pindah scroll
  $('html , body').animate({
   scrollTop: elemenTujuan.offset().top - 50
@@ -14,3 +11,43 @@ $('.page-scroll').on('click', function(e) {
 
  e.preventDefault();
 });
+
+
+//paralax
+//about
+
+$(window).on('load', function() {
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+});
+
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	
+//jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+	
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+
+	//portfolio
+	if( wScroll > $('.portfolio').offset().top -500 ) {
+		$('.portfolio .thumbnail').each(function(i){
+			setTimeout (function() {
+				$('.portfolio .thumbnail').eq(i).addClass('muncul');
+			},300 * (i+1));
+		});
+	}
+	
+	
+	
+});
+
+
